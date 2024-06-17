6ba27fe7fe9ed8eb9d6b5c5961c87f2bb2449960
Philbert <philbertshea@gmail.com>
2024-06-18 00:00:53 +0800

    update chromedriver version

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def download_selenium():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version='114.0.5735.90').install()), options=chrome_options)
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version='126.0.6478.61').install()), options=chrome_options)
     driver.get("https://google.com")
     title = driver.title
     language = driver.find_element(By.XPATH, "//div[@id='SIvCob']").text
