fed8eaff70cd2b9e876498e969b3453fa93860cd
Philbert <philbertshea@gmail.com>
2024-06-17 23:09:21 +0800

    update main

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ app = Flask(__name__)
 def download_selenium():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    prefs = {"profile.managed_default_content_settings.images": 2}
-    chrome_options.add_experimental_option("prefs", prefs)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://google.com")
     title = driver.title
     language = driver.find_element(By.XPATH, "//div[@id='SIvCob']").text
@@ -26,7 +22,7 @@ def download_selenium():
 @app.route('/', methods = ['GET','POST'])
 def home():
     if (request.method == 'GET'):
-        return "Hi"
+        return download_selenium()
 
 
 if __name__ == "__main__":
