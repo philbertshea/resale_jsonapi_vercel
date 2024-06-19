3560627f123ef641392e876d1d8236f1b7c02a4a
Philbert <philbertshea@gmail.com>
2024-06-19 23:17:29 +0800

    edit main

--- main.py
+++ main.py
@@ -38,5 +38,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=8000))
     
