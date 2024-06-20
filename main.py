55662f957153205753af445a945010dfafecd64a
philbertshea <85420849+philbertshea@users.noreply.github.com>
2024-06-20 09:24:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,5 +37,6 @@ def home():
         return download_selenium()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     
