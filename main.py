86f46c9b83687b86b66fcd01754f411851536c1b
Philbert <philbertshea@gmail.com>
2024-06-22 22:06:26 +0800

    add remotedebug and disgpu

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def hello():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--remote-debugging-port=9230')
+    options.add_argument('--disable-gpu')
     options.binary_location = '/usr/local/bin/google-chrome'
 
     # Specify the path to the ChromeDriver executable
